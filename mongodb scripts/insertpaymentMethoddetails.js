5cf0b74526d30de5262ba115192d8da735307338
Mohammad Tarique Anjum <190031065@kluniversity.in>
2020-10-31 13:17:19 +0530

    Insert Payment Method

---
+++ mongodb scripts/insertpaymentMethoddetails.js
@@ -0,0 +1,34 @@
+db.paymentmethods.insert(
+    [
+        {
+            "Type":"debitcard",
+            "Card Number":"1234 5678 9012 3456",
+            "Card Holder Name":"Veer Singh K",
+            "Expiry":"11/24",
+            "PaymentNetwork":"visa",
+            "CVV":"213"
+        },
+        {
+            "Type":"netbanking",
+            "Account Number":"1244 1878 1232 1464",
+            "Account Holder Name":"Sastry Kumar V",
+            "Bank":"SBI"
+        },
+        {
+            "Type":"creditcard",
+            "Card Number":"1144 1678 9012 3456",
+            "Card Holder Name":"Vinod  K",
+            "Expiry":"11/22",
+            "PaymentNetwork":"Master Card",
+            "CVV":"212"
+        },
+        {
+            "Type":"debitcard",
+            "Card Number":"9112 6456 3142 7678 ",
+            "Card Holder Name":"Rahul V",
+            "Expiry":"01/21",
+            "PaymentNetwork":"visa",
+            "CVV":"177"
+        }
+    ]
+);
