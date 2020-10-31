5b86367e5bcb731dba4a412b4f4c1bc57453db5e
Togaru Joseph Subhakar <190031634@kluniversity.in>
2020-10-31 11:04:29 +0530

    sign up details

---
+++ mongodb scripts/signupdetails.js
@@ -0,0 +1,12 @@
+[
+{"FirstName":"ramarao","LastName":"thota","Username":"ramaroa1234","Email":"ramaro9898@gmail.com","mobileno":"9000009898","passWord":"ramaroa9898"},
+{"FirstName":"rajsekhar","LastName":"tambi","Username":"rajtambi4","Email":"tambiraj98@gmail.com","mobileno":"9000709898","passWord":"rajsekhar900"},
+{"FirstName":"kiranmay","LastName":"irinki","Username":"kiranmayirinki","Email":"kiranmay143@gmail.com","mobileno":"9600009898","passWord":"irinki2000"},
+{"FirstName":"tarique","LastName":"mohamad","Username":"tariquemd","Email":"tariquemd10@gmail.com","mobileno":"9550009898","passWord":"tariqueanjum"},
+{"FirstName":"vamsi","LastName":"krishna","Username":"vamsihero007","Email":"vamsikrishna@gmail.com","mobileno":"9007809898","passWord":"marvelvamsi"},
+{"FirstName":"arunakumari","LastName":"gali","Username":"arunagali","Email":"galiarunakumari@gmail.com","mobileno":"989899898","passWord":"sridevi"},
+{"FirstName":"naveen","LastName":"jedi","Username":"naveen3209","Email":"jedinaveen@gmail.com","mobileno":"9987009898","passWord":"naveenjedi123"},
+{"FirstName":"subhakar","LastName":"nandi","Username":"nandisubhakar","Email":"subhakarnn@gmail.com","mobileno":"9097509898","passWord":"subhakarnandi"},
+{"FirstName":"deepak","LastName":"raj","Username":"deepakraj420","Email":"deepunn98@gmail.com","mobileno":"9265009898","passWord":"deepakraj007"},
+{"FirstName":"harish","LastName":"madha","Username":"harishmadha99","Email":"harish100@gmail.com","mobileno":"9034509898","passWord":"Harishdc"},
+]
