09de9d18b0429a21dcc3836b197bff4aff12e7a7
Mohana Vamsi Batchu <190030141@kluniversity.in>
2020-10-31 13:42:52 +0530

    Insert Cart details

---
+++ mongodb scripts/cart.js
@@ -0,0 +1,10 @@
+db.cart.insert([{product:"Nike Shoes",Quantity:"1",price:"499rs"},
+	{product:"Samsung mobile",Quantity:"1",price:"30000rs"},
+	{product:"Beats headphones",Quantity:"1",price:"9000rs"},
+	{product:"Jeans",Quantity:"1",price:"799rs"},
+	{product:"Nike shirts",Quantity:"3",price:"999rs"},
+	{product:"woodland shoes",Quantity:"1",price:"800rs"},
+	{product:"uspolo shirts",Quantity:"2",price:"2999rs"},
+	{product:"lenovo laptop",Quantity:"1",price:"49000rs"},
+	{product:"face masks",Quantity:"4",price:"400rs"}
+]);
