591cdcb21064a0a526ddf79a01777c931f1c16cd
Mohana Vamsi Batchu <190030141@kluniversity.in>
2020-10-31 13:43:26 +0530

    Insert Product details

---
+++ mongodb scripts/product.js
@@ -0,0 +1,9 @@
+db.product.insert([{product:"Nike Shoes",catogory:"clothing",sizes:"8,9,10",stock:"InStock",colors:"Red,Black,Blue",rating:"4.5/5",discount:"3%",price:"499rs"},
+	{product:"Samsung mobile",catogory:"Electronics",screen_length:"5 Inchs",stock:"InStock",colors:"Black,white",rating:"3.5/5",discount:"2%",price:"30000rs"},
+	{product:"Beats headphones",catogory:"Electronics",type:"Bluetooth",stock:"InStock",colors:"Black,Blue,Green",rating:"4.5/5",discount:"5%",price:"9000rs"},
+	{product:"Jeans",catogory:"Clothings",sizes:"30,32,36,40",stock:"InStock",colors:"Blue,Black",rating:"4.5/5",discount:"2%",price:"799rs"},
+	{product:"Nike shirts",catogory:"Clothing",sizes:"S,M,L,XL",stock:"InStock",colors:"Indiho,violet,red,yellow",rating:"3.8/5",discount:"10%",price:"333rs"},
+	{product:"Woodland Shoes",catogory:"Clothing",sizes:"8,9,10",stock:"OutofStock",colors:"brown",rating:"4.0/5",discount:"5%",price:"800rs"},
+	{product:"uspolo shirts",catogory:"clothing",sizes:"S,M,L,XL,XXL",stock:"Instock",colors:"blue",rating:"4.3/5",discount:"3%",price:"1489rs"},
+	{product:"lenovo laptop",catogory:"Electronics",processor:"8gb ram 2b graphics",storage:"1TB",stock:"inStock",colors:"Gray",rating:"4.3/5",discount:"5%",price:"49000rs"}
+]);
