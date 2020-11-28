babf1f6ad750c186d7d58e65abdcfb6d9474c78a
iamtariqueanjum <tariqueanjummd9302@gmail.com>
2020-11-28 18:31:42 +0530

    Form restrictions added

--- code/src/app/app.component.spec.ts
+++ code/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'code'`, () => {
+  it(`should have as title 'Estore'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('code');
+    expect(app.title).toEqual('Estore');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('code app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Estore app is running!');
   });
 });
